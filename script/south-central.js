9991ddfec4c71f8ad2c4d0a0f986875c743bff2c
rainbowlarva <86572604+rainbowlarva@users.noreply.github.com>
2025-06-12 09:09:44 -0700

    Update south-central.js

--- script/south-central.js
+++ script/south-central.js
@@ -55,7 +55,7 @@ function generateBBCode(e) {
   const timeDate       = document.getElementById('timeDate').value.trim()      || 'N/A';
   const location       = document.getElementById('location').value.trim()      || 'N/A';
   const officerName   = document.getElementById('officerName').value.trim()  || 'N/A';
-  const departmentRank = document.getElementById('departmentRank').value.trim()|| 'N/A';
+  const officerRank = document.getElementById('officerRank').value.trim()|| 'N/A';
   const narrative      = document.getElementById('narrative').value.trim()     || 'N/A';
   const attachments    = document.getElementById('attachments').value.trim()   || 'N/A';
   const employeeSig    = document.getElementById('employeeSig').value.trim()   || 'N/A';
@@ -77,7 +77,7 @@ function generateBBCode(e) {
 
   finalText = finalText.replace(
     '[b]EMPLOYEE FULL NAME:[/b] \n[b]DEPARTMENTAL RANK:[/b] \n',
-    `[b]EMPLOYEE FULL NAME:[/b] ${officerName}\n[b]DEPARTMENTAL RANK:[/b] ${departmentRank}\n`
+    `[b]EMPLOYEE FULL NAME:[/b] ${officerName}\n[b]DEPARTMENTAL RANK:[/b] ${officerRank}\n`
   );
 
   finalText = finalText.replace('Full Incident / Intelligence narrative here â€¦', narrative);
